6d9e22df0ac968ba78feb827225b8605bd17928e
Fabian Furger <furgerfabian@hotmail.com>
2015-03-29 11:12:31 +0100

    added some structure

---
+++ Ai2dShooter/View/MainForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Ai2dShooter.View
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
